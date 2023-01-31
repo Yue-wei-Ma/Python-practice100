c5e3f5806ae9d75c6b12c2436076f1c7e08eb5c4
YueweiMa <108724061+Yue-wei-Ma@users.noreply.github.com>
2023-01-31 11:40:53 +0800

    Add files via upload

---
+++ 42_auto定义变量.py
@@ -0,0 +1,13 @@
+"""
+题目：学习使用auto定义变量的用法
+明白输出的结果
+"""
+num=2
+def autofunc():
+    num=1
+    print('internal block num=%d' %num)
+    num+=1
+for i in range(3):
+    print('The num=%d' %num)
+    num+=1
+    autofunc()
